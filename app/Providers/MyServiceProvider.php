fbf76791ca15a30dfeed00b8068072f1b7609742
Roberto Butti <roberto.butti@gmail.com>
2023-01-19 22:14:14 +0100

    Update MyServiceProvider.php

--- app/Providers/MyServiceProvider.php
+++ app/Providers/MyServiceProvider.php
@@ -10,19 +10,16 @@ class MyServiceProvider extends ServiceProvider
     public function __construct($app)
     {
         parent::__construct($app);
-        //Log::debug("__METHOD__");
         Log::debug("CONSTRUCT");
     }
 
     public function register()
     {
-        // Log::debug(__METHOD__);
         Log::debug("REGISTER");
     }
 
     public function boot()
     {
-        // Log::debug(__METHOD__);
         Log::debug("BOOT");
     }
 }
