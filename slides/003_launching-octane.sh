c15858ed91d290e8098f9866feb89fc89b8b5531
Roberto Butti <roberto.butti@gmail.com>
2023-01-20 07:38:04 +0100

    Create 003_launching-octane.sh

---
+++ slides/003_launching-octane.sh
@@ -0,0 +1,7 @@
+# Starting the classical PHP built-in Web server
+php artisan serve
+# Starting Octane service
+php artisan octane:start
+# Starting Octane service, reloading after 10 requests
+php artisan octane:start --max-requests=10
+
